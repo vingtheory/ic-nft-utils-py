233e882f0bcee928d455cbd39c80fa4cabc5369f
vingtheory <112404892+vingtheory@users.noreply.github.com>
2022-12-17 18:09:12 -0600

    fixed timeout issue by adding auto-retry and improved console logging

--- CCCCanisterUtils.py
+++ CCCCanisterUtils.py
@@ -17,59 +17,118 @@ class CCCNFTCanisterUtils:
         self.canister = Canister(agent=agent, canister_id=inputSrcNftCanisterId, candid=candid)
 
     def downloadAllTokensMetadata(self):
+        print('Starting - downloadAllTokensMetadata...')
         tokenIndex = 0
         tokenDetails = []
+        iterationSuccess = 0
         while tokenIndex < self.totalTokensSupply:
-             res = self.canister.getTokenById(tokenIndex + 1)
-             tokenDetails.append(res)
-             tokenIndex = tokenIndex + 1
+            try:
+                # print('tokenIndex: ', tokenIndex)
+                res = self.canister.getTokenById(tokenIndex)
+                iterationSuccess = 1
+            except KeyboardInterrupt: 
+                print('User Keyboard Interrupt occured - downloadAllTokensMetadata. Will exit.')
+                return
+            except:
+                print('Exception occured - downloadAllTokensMetadata. Will continue and try again.')
+                continue
+            finally:
+                if (iterationSuccess == 1):
+                    tokenDetails.append(res)
+                    tokenIndex = tokenIndex + 1
+                    iterationSuccess = 0
         FileIO.writeToJSON('tokensMetadata', tokenDetails)
+        print('Successful - downloadAllTokensMetadata.')
 
     def uploadAllTokensMetadata(self):
         tokensMetadata = FileIO.readFromJSON('tokensMetadata')
         print(tokensMetadata)
 
     def downloadAllComponentsInfo(self):
+        print('Starting - downloadAllComponentsInfo...')
         componentSize = self.canister.getComponentsSize()
         componentsDetails = []
-        index = 0
-        while index < componentSize[0]:
-             res = self.canister.getComponentById(index)
-             componentsDetails.append(res)
-             index = index + 1
+        tokenIndex = 0
+        while tokenIndex < componentSize[0]:
+            try:
+                # print('tokenIndex: ', tokenIndex)
+                res = self.canister.getComponentById(tokenIndex)
+                iterationSuccess = 1
+            except KeyboardInterrupt: 
+                print('User Keyboard Interrupt occured - downloadAllComponentsInfo. Will exit.')
+                return
+            except:
+                print('Exception occured - downloadAllComponentsInfo. Will continue and try again.')
+                continue
+            finally:
+                if (iterationSuccess == 1):
+                    componentsDetails.append(res)
+                    tokenIndex = tokenIndex + 1
+                    iterationSuccess = 0
         FileIO.writeToJSON('componentsDetails', componentsDetails)
+        print('Successful - downloadAllComponentsInfo.')
 
     def uploadAllComponentsInfo(self):
         componentsDetails = FileIO.readFromJSON('componentsDetails')
         print(componentsDetails)
 
     def downloadAllListings(self):
+        print('Starting - downloadAllListings...')
         listings = self.canister.getListings()
         FileIO.writeToPickle('listings', listings[0])
+        print('Successful - downloadAllListings.')
 
     def uploadAllListings(self):
         listings = FileIO.readFromPickle('listings')
 
     def downloadAllSoldListings(self):
+        print('Starting - downloadAllSoldListings...')
         soldListings = self.canister.getSoldListings()
         FileIO.writeToPickle('soldListings', soldListings[0])
+        print('Successful - downloadAllSoldListings.')
 
     def uploadAllSoldListings(self):
         soldListings = FileIO.readFromPickle('soldListings')
 
+    # iterate through all nfts index and download one by one - time consuming. use getRegistry instead
     def downloadAllTokenAndOwnerMetadata(self):
+        print('Starting - downloadAllTokenAndOwnerMetadata...')
         tokenIndex = 0
         owners = []
+        iterationSuccess = 0
         while tokenIndex < self.totalTokensSupply:
-             mintNumber = tokenIndex + 1 
-             res = self.canister.ownerOf(mintNumber)
-             owners.append({'mintNumber': mintNumber, 'owner': res[0][0].to_str()})
-             tokenIndex = tokenIndex + 1
+            try:
+                # print('tokenIndex: ', tokenIndex)
+                res = self.canister.ownerOf(tokenIndex)
+                iterationSuccess = 1
+            except KeyboardInterrupt: 
+                print('User Keyboard Interrupt occured - downloadAllTokenAndOwnerMetadata. Will exit.')
+                return
+            except:
+                print('Exception occured - downloadAllTokenAndOwnerMetadata. Will continue and try again.')
+                continue
+            finally:
+                if (iterationSuccess == 1):
+                    owners.append({'tokenIndex': tokenIndex, 'owner': res[0][0].to_str()})
+                    tokenIndex = tokenIndex + 1
+                    iterationSuccess = 0
         FileIO.writeToJSON('owners', owners)
+        print('Successful - downloadAllTokenAndOwnerMetadata.')
 
     def uploadAllTokenAndOwnerMetadata(self):
         owners = FileIO.readFromJSON('owners')
         print(owners)
+
+    # mapping user-principal to nft index
+    def downloadAllTokenAndOwnerMetadata_faster(self):
+        print('Starting - downloadAllTokenAndOwnerMetadata_faster...')
+        listings = self.canister.getRegistry()
+        FileIO.writeToPickle('owners_faster', listings[0])
+        print('Successful - downloadAllTokenAndOwnerMetadata_faster.')
+
+    def uploadAllTokenAndOwnerMetadata_faster(self):
+        data = FileIO.readFromJSON('owners_faster')
+        print(data)
 
 # image
 class CCCNFTMetadataCanisterUtils:
@@ -83,15 +142,29 @@ class CCCNFTMetadataCanisterUtils:
         self.canister = Canister(agent=agent, canister_id=inputSrcMetadataCanisterId, candid=candid)
 
     def downloadAllImages(self):
+        print('Starting - downloadAllImages...')
         tokenIndex = 0
         images = []
+        iterationSuccess = 0
         while tokenIndex < self.totalTokensSupply:
-             mintNumber = tokenIndex + 1 
-             httpRequest = { 'body' : [], 'headers': [], 'method' : 'get', 'url':f"token/{mintNumber}"}
-             res = self.canister.http_request(httpRequest)
-             images.append({'mintNumber': mintNumber, 'image': res[0]})
-             tokenIndex = tokenIndex + 1
+            try:
+                # print('tokenIndex: ', tokenIndex)
+                httpRequest = { 'body' : [], 'headers': [], 'method' : 'get', 'url':f"token/{tokenIndex}"}
+                res = self.canister.http_request(httpRequest)
+                iterationSuccess = 1
+            except KeyboardInterrupt: 
+                print('User Keyboard Interrupt occured - downloadAllImages. Will exit.')
+                return
+            except:
+                print('Exception occured - downloadAllImages. Will continue and try again.')
+                continue
+            finally:
+                if (iterationSuccess == 1):
+                    images.append({'tokenIndex': tokenIndex, 'image': res[0]})
+                    tokenIndex = tokenIndex + 1
+                    iterationSuccess = 0
         FileIO.writeToPickle('images', images)
+        print('Successful - downloadAllImages.')
 
     def uploadAllImages(self):
         images = FileIO.readFromPickle('images')
@@ -108,16 +181,42 @@ class CCCNFTStorageCanisterUtils:
         agent = Agent(identity, client)
         self.canister = Canister(agent=agent, canister_id=inputSrcStorageCanisterId, candid=candid)
 
+    # download all sales transaction for each token index - slower. use downloadAllTransactions_faster instead
     def downloadAllTransactions(self):
+        print('Starting - downloadAllTransactions...')
         tokenIndex = 0
         history = []
+        iterationSuccess = 0
         while tokenIndex < self.totalTokensSupply:
-             mintNumber = tokenIndex + 1 
-             res = self.canister.getHistory(mintNumber)
-             history.append({'mintNumber': mintNumber, 'history': res[0]})
-             tokenIndex = tokenIndex + 1
+            try:
+                # print('tokenIndex: ', tokenIndex)
+                res = self.canister.getHistory(tokenIndex)
+                iterationSuccess = 1
+            except KeyboardInterrupt: 
+                print('User Keyboard Interrupt occured - downloadAllTransactions. Will exit.')
+                return
+            except:
+                print('Exception occured - downloadAllTransactions. Will continue and try again.')
+                continue
+            finally:
+                if (iterationSuccess == 1):
+                    history.append({'tokenIndex': tokenIndex, 'history': res[0]})
+                    tokenIndex = tokenIndex + 1
+                    iterationSuccess = 0
         FileIO.writeToPickle('history', history)
+        print('Successful - downloadAllTransactions.')
 
     def uploadAllTransactions(self):
         history = FileIO.readFromPickle('history')
         print(history)
+
+    # download all sales transaction in one dump
+    def downloadAllTransactions_faster(self):
+        print('Starting - downloadAllTransactions_faster...')
+        listings = self.canister.getAllSaleRecord()
+        FileIO.writeToPickle('history_faster', listings[0])
+        print('Successful - downloadAllTransactions_faster.')
+
+    def uploadAllTransactions_faster(self):
+        data = FileIO.readFromJSON('history_faster')
+        print(data)
--- gangs_nft.py
+++ gangs_nft.py
@@ -1,27 +1,50 @@
 from CCCCanisterUtils import *
 
-srcNftCanisterId = '2x3lw-tiaaa-aaaah-qcvca-cai'
+srcNftCanisterId = '2x3lw-tiaaa-aaaah-qcvca-cai' 
 srcMetadataCanisterId = 'v4oyv-zaaaa-aaaah-qctya-cai'
 srcStorageCanisterId = '2q2nc-6qaaa-aaaah-qcvcq-cai'
 totalTokensSupply = 10000
 
 cccNFTCanisterUtils = CCCNFTCanisterUtils('dfinityGangs', srcNftCanisterId, totalTokensSupply)
-cccNFTCanisterUtils.downloadAllTokenAndOwnerMetadata()
-cccNFTCanisterUtils.uploadAllTokenAndOwnerMetadata()
-cccNFTCanisterUtils.downloadAllTokenAndOwnerMetadata()
-cccNFTCanisterUtils.downloadAllListings()
-cccNFTCanisterUtils.uploadAllListings()
-cccNFTCanisterUtils.downloadTokensMetadata()
-cccNFTCanisterUtils.downloadAllComponentsInfo()
-cccNFTCanisterUtils.uploadAllComponentsInfo()
+
+print('JOB STARTING.')
+
+print('(1/7) - Downloading owners and holdings of all tokens...')
+# owners and holdings of all tokens
+cccNFTCanisterUtils.downloadAllTokenAndOwnerMetadata_faster()
+# cccNFTCanisterUtils.uploadAllTokenAndOwnerMetadata_faster()
+
+print('(2/7) - Downloading all tokens - metadata, traits, rarity...')
+# all tokens - metadata, traits, rarity
 cccNFTCanisterUtils.downloadAllTokensMetadata()
-cccNFTCanisterUtils.uploadAllTokensMetadata()
+# cccNFTCanisterUtils.uploadAllTokensMetadata()
+
+print('(3/7) - Downloading all component info - all trait and rarity...')
+# all component info - all trait and rarity 
+cccNFTCanisterUtils.downloadAllComponentsInfo()
+# cccNFTCanisterUtils.uploadAllComponentsInfo()
+
+print('(4/7) - Downloading current listings...')
+# current listings
+cccNFTCanisterUtils.downloadAllListings()
+# cccNFTCanisterUtils.uploadAllListings()
+
+print('(5/7) - Downloading sold listings...')
+# sold listings
+cccNFTCanisterUtils.downloadAllSoldListings()
+# cccNFTCanisterUtils.uploadAllSoldListings()
 
 cccNFTMetadataCanisterUtils = CCCNFTMetadataCanisterUtils('dfinityGangs', srcMetadataCanisterId, totalTokensSupply)
+
+print('(6/7) - Downloading all images...')
 cccNFTMetadataCanisterUtils.downloadAllImages()
-cccNFTMetadataCanisterUtils.uploadAllImages()
+# cccNFTMetadataCanisterUtils.uploadAllImages()
 
 cccStorageCanisterUtils = CCCNFTStorageCanisterUtils('dfinityGangs', srcStorageCanisterId, totalTokensSupply)
-cccStorageCanisterUtils.downloadAllTransactions()
-cccStorageCanisterUtils.uploadAllTransactions()
+
+print('(7/7) - Downloading all transactions...')
+cccStorageCanisterUtils.downloadAllTransactions_faster()
+# cccStorageCanisterUtils.uploadAllTransactions_faster()
+
+print('JOB COMPLETE.')
 
